f39ae69f844bccb715d3ebd80553c38ce503728c
Alan <xujiaoweialan@gmail.com>
2015-11-20 15:40:09 +0800

    创建项目

---
+++ app/src/main/java/com/alan/propertyanimation/MainActivity.java
@@ -0,0 +1,15 @@
+package com.alan.propertyanimation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
